4803c97d29a7ad89f546c1372cf250a24eb9736f
ssebastiancardona <ss3bastiancardona@gmail.com>
2022-07-16 13:16:48 -0500

    task_2/

---
+++ 0x09-react_redux_reducer_selector/task_2/dashboard/src/reducer/uiReducer.js
@@ -0,0 +1,52 @@
+import {
+    LOGIN,
+    LOGOUT,
+    DISPLAY_NOTIFICATION_DRAWER,
+    HIDE_NOTIFICATION_DRAWER,
+    LOGIN_SUCCESS,
+    LOGIN_FAILURE,
+  } from '../actions/uiActionTypes';
+  import Immutable from 'immutable';
+
+  export const initialState = {
+    isNotificationDrawerVisible: false,
+    isUserLoggedIn: false,
+    user: {},
+  }
+
+  export const uiReducer = (state = new Immutable.Map(initialState), action) => {
+    switch (action.type) {
+        case DISPLAY_NOTIFICATION_DRAWER: {
+          return {
+            ...state,
+            isNotificationDrawerVisible: true,
+          };
+        };
+        case HIDE_NOTIFICATION_DRAWER: {
+          return {
+            ...state,
+            isNotificationDrawerVisible: false,
+          };
+        };
+        case LOGIN_SUCCESS: {
+          return {
+            ...state,
+            isUserLoggedIn: true,
+          };
+        };
+        case LOGIN_FAILURE: {
+          return {
+            ...state,
+            isUserLoggedIn: false,
+          };
+        };
+        case LOGOUT: {
+          return {
+            ...state,
+            isUserLoggedIn: false,
+          };
+        };
+        default:
+          return state;
+      }
+  }
